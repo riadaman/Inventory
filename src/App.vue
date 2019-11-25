12431cca74ea2d3c5e4177e22ec7e7b236abd563
backslesh420 <riadmohammod2000@gmail.com>
2019-11-25 21:14:25 +0600

    create layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Inventory</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="container">
+<div class="row">
+  <div class="col-sm-9">
+    <div class="card" style="width: 18rem;">
+    <img src="" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <div class="col-sm-3">
+    <ul class="list-group">
+      <li class="list-group-item">
+        Name - Price
+      </li>
+    </ul>
+  </div>
+</div>
+  </div>
   </div>
 </template>
 
@@ -12,5 +47,7 @@ export default {
 </script>
 
 <style>
-
+ .container {
+   margin-top: 50px;
+ }
 </style>
